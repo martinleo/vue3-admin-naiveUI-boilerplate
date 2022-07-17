adc66ed4ab3b5a4a6aaf54cb8b9d48830752bae6
vagrant <vagrant@vagrant.vm>
2022-07-17 11:04:25 +0000

    changes

--- src/stores/app.js
+++ src/stores/app.js
@@ -1,7 +1,6 @@
 import { defineStore } from "pinia";
 
-// useStore could be anything like useUser, useCart
-// the first argument is a unique id of the store across your application
+
 export const useApp = defineStore("app", {
   state: () => ({
     isLoading: true,
